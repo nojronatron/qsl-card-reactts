4a239076b933e42628dd3e620f6dc32774363c02
Jon Rumsey <jonrumsey.dev@gmail.com>
2024-11-13 18:10:29 -0800

    bootstrap project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>K7RMZ QSL Card Generator</h1>
+      <div className="card">
+        A JR Software project by Nojronatron
+      </div>
+      <img src="jrsoftware-logo-128x128.png" alt="JR Software Logo" width="128px" />
+    </>
+  )
+}
+
+export default App
