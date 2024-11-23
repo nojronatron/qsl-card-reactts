930f83006948329faf8ae4df8c3dee430713a24c
Jon Rumsey <jonrumsey.dev@gmail.com>
2024-11-23 10:30:27 -0800

    Refactor header, footer into Components.

---
+++ src/components/FooterComponent.tsx
@@ -0,0 +1,7 @@
+function FooterComponent() {
+  return (
+  <div className='footer-text'>Author: K7RMZ aka Nojronatron</div>
+  );
+}
+
+export default FooterComponent;
---
+++ src/components/HeaderComponent.tsx
@@ -0,0 +1,10 @@
+function HeaderComponent() {
+  return (
+    <div className='header-nav'>
+    <img src="jrsoftware-logo-128x128.png" alt="JR Software Logo" width="32px" />
+    <p>QSL Card Generator</p>
+  </div>
+  );
+}
+
+export default HeaderComponent;
