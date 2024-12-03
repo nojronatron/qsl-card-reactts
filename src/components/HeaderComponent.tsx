6e28f9f1abe27be6a6a4412256017a19069b80ee
Jon Rumsey <jonrumsey.dev@gmail.com>
2024-12-03 15:41:42 -0800

    Link up jrsoftware logo

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,12 +1,18 @@
 function HeaderComponent() {
   return (
     <header className='header-nav'>
-      <img
-        src='jrsoftware-logo-128x128.png'
-        alt='JR Software Logo'
-        width='32px'
-      />
-      <p>QSL Card Generator</p>
+      <a
+        href='https://github.com/nojronatron'
+        target='_blank'
+        rel='noreferrer noopener'
+      >
+        <img
+          src='src/assets/jrsoftware-logo-128x128.png'
+          alt='JR Software Logo links to GitHub'
+          className='header-logo'
+        />
+      </a>
+      <p className='header-title'>QSL Card Generator</p>
     </header>
   );
 }
